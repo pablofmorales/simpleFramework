f3f4326b29473ec0a432cfa520301fa8266d7404
Pablo Morales <pablofmorales@gmail.com>
2016-01-05 18:50:44 -0300

    Allowing use GET/POST params

--- src/Framework/Application.php
+++ src/Framework/Application.php
@@ -11,6 +11,16 @@ class Application
         'OPTION' => [],
     ];
 
+    public $postValues = [];
+    public $getValues = [];
+
+    public function __construct($server, $post, $get)
+    {
+        $this->server = $server;
+        $this->postValues = $post;
+        $this->getValues = $get;
+    }
+
     public function post($route, $controller)
     {
         $this->routes['POST'][$route] = $controller;
@@ -21,13 +31,11 @@ class Application
         $this->routes['GET'][$route] = $controller;
     }
 
-    public function run($server)
+    public function run()
     {
-        $route = $this->routes[$server['REQUEST_METHOD']];
-        if (\array_key_exists($server['REQUEST_URI'], $route)) {
-            return $route[$server['REQUEST_URI']]();
+        $route = $this->routes[$this->server['REQUEST_METHOD']];
+        if (\array_key_exists($this->server['REQUEST_URI'], $route)) {
+            return $route[$this->server['REQUEST_URI']]();
         }
-
-
     }
 }
--- tests/Unit/ApplicationTest.php
+++ tests/Unit/ApplicationTest.php
@@ -5,7 +5,8 @@ class Application extends \PHPUnit_Framework_TestCase
 
     public function testGetValidMethodShouldReturnValidOutput()
     {
-        $app = new Framework\Application();
+        $server = ['REQUEST_URI' => '/home', 'REQUEST_METHOD' => 'GET'];
+        $app = new Framework\Application($server, [], []);
         $app->get('/home', function() {
             return 'test';
         });
@@ -15,13 +16,30 @@ class Application extends \PHPUnit_Framework_TestCase
 
     public function testPostValidMethodShouldReturnValidOutput()
     {
-        $app = new Framework\Application();
+        $server = ['REQUEST_URI' => '/home', 'REQUEST_METHOD' => 'POST'];
+        $app = new Framework\Application($server, [], []);
         $app->post('/home', function() {
             return 'test';
         });
-        $server = ['REQUEST_URI' => '/home', 'REQUEST_METHOD' => 'POST'];
-        $this->assertEquals('test', $app->run($server));
+        $this->assertEquals('test', $app->run());
+    }
 
+    public function testMethodNeedToReadPostAndGetParameters()
+    {
+        $server = ['REQUEST_URI' => '/home', 'REQUEST_METHOD' => 'POST'];
+        $app = new Framework\Application($server, ['val' => 1], ['val' => 2]);
+        $app->post('/home', function() use ($app) {
+            $response = [
+                'get' => $app->getValues,
+                'post' => $app->postValues,
+            ];
+            return \json_encode($response);
+        });
+        $expected = \json_encode([
+            'get' => ['val' => 2],
+            'post' => ['val' => 1],
+        ]);
+        $this->assertEquals($expected, $app->run());
     }
 
 }
