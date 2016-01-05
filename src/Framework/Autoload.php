e6ba97e7a60fad389832686924be5c45bb313fb8
Pablo Morales <pablofmorales@gmail.com>
2016-01-05 11:48:28 -0300

    Not need exception when you don't found the Class

--- src/Framework/Autoload.php
+++ src/Framework/Autoload.php
@@ -1,15 +1,12 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    $baseDir = __DIR__ . '/src/';
+    $baseDir = __DIR__ . '/../../src/';
 
     $file = $baseDir . str_replace('\\', '/', $class) . '.php';
 
     if (file_exists($file)) {
         require $file;
     }
-
-    throw new \InvalidArgumentException('File {$file} is not exists');
-
 });
 
