7604538f7109dea9b600c6d9f2aaf8d4d29fafe6
Pablo Morales <pablofmorales@gmail.com>
2016-01-05 18:31:06 -0300

    Adding routes depending on the method

--- src/Framework/Application.php
+++ src/Framework/Application.php
@@ -4,18 +4,30 @@ namespace Framework;
 
 class Application
 {
-    private $routes = [];
+    private $routes = [
+        'GET' => [],
+        'POST' => [],
+        'PATCH' => [],
+        'OPTION' => [],
+    ];
+
+    public function post($route, $controller)
+    {
+        $this->routes['POST'][$route] = $controller;
+    }
 
     public function get($route, $controller)
     {
-        $this->routes[$route] = $controller;
+        $this->routes['GET'][$route] = $controller;
     }
-
 
     public function run($server)
     {
-        if (\array_key_exists($server['uri'], $this->routes)) {
-            return $this->routes[$server['uri']]();
+        $route = $this->routes[$server['REQUEST_METHOD']];
+        if (\array_key_exists($server['REQUEST_URI'], $route)) {
+            return $route[$server['REQUEST_URI']]();
         }
+
+
     }
 }
--- tests/Unit/ApplicationTest.php
+++ tests/Unit/ApplicationTest.php
@@ -3,16 +3,25 @@
 class Application extends \PHPUnit_Framework_TestCase
 {
 
-    public function testGetValidMethodShouldReturnValidInformation()
+    public function testGetValidMethodShouldReturnValidOutput()
     {
-
         $app = new Framework\Application();
-
         $app->get('/home', function() {
             return 'test';
         });
-
-        $server = ['uri' => '/home'];
+        $server = ['REQUEST_URI' => '/home', 'REQUEST_METHOD' => 'GET'];
         $this->assertEquals('test', $app->run($server));
     }
+
+    public function testPostValidMethodShouldReturnValidOutput()
+    {
+        $app = new Framework\Application();
+        $app->post('/home', function() {
+            return 'test';
+        });
+        $server = ['REQUEST_URI' => '/home', 'REQUEST_METHOD' => 'POST'];
+        $this->assertEquals('test', $app->run($server));
+
+    }
+
 }
