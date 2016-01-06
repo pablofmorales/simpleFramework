1f55afccfedb7f27d7614dca6a6a6c89100ad87b
Pablo Morales <pablofmorales@gmail.com>
2016-01-06 18:22:05 -0300

    Allow autoincrement with collections

--- src/Framework/Storage/Drivers/Collection.php
+++ src/Framework/Storage/Drivers/Collection.php
@@ -13,7 +13,23 @@ class Collection
 
     public function insert($name, $data)
     {
-        $this->collection[$name][] = $data;
+        $index = $this->getIndex($name);
+        $this->collection[$name][$index] = $data;
         return true;
+    }
+
+    private function getIndex($name)
+    {
+        if (! isset($this->collection[$name])) {
+            return 1;
+        }
+        $elements = count($this->collection[$name]);
+
+        return ++ $elements;
+    }
+
+    public function fetch($name, $id)
+    {
+        return $this->collection[$name][$id];
     }
 }
--- tests/Unit/StorageService/CollectionTest.php
+++ tests/Unit/StorageService/CollectionTest.php
@@ -5,14 +5,30 @@ class Collection extends \PHPUnit_Framework_TestCase
 
     public function testTryToInsertReturnTrue()
     {
+        $expected = [
+            1 => ['test' => 1],
+            2 => ['test' => 2]
+        ];
+        $driver = new Framework\Storage\Drivers\Collection();
+        $storage = new Framework\Storage($driver);
+        $storage->insert('dumyCollection', $expected[1]);
+        $storage->insert('dumyCollection', $expected[2]);
+
+        $collection = $storage->fetchAll('dumyCollection');
+
+        $this->assertEquals($expected, $collection);
+    }
+
+    public function testGivenAnIdShouldReturnAnItem()
+    {
         $expected = ['test' => 1];
         $driver = new Framework\Storage\Drivers\Collection();
         $storage = new Framework\Storage($driver);
         $storage->insert('dumyCollection', $expected);
 
-        $collection = $storage->fetchAll('dumyCollection');
+        $collection = $storage->fetch('dumyCollection', 1);
 
-        $this->assertEquals([$expected], $collection);
+        $this->assertEquals($expected, $collection);
     }
 
 }
